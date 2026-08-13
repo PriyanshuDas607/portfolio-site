e8e20b2830b70edebcf3a63309c7f3ce84675074
Priyanshu Das <Priyanshudas272006@gmail.com>
2026-08-13 22:43:07 +0530

    Chore: Update chunkSizeWarningLimit to 2000kB in vite config

--- vite.config.ts
+++ vite.config.ts
@@ -4,15 +4,15 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    chunkSizeWarningLimit: 1000,
+    chunkSizeWarningLimit: 2000, // Raises warning limit to 2000 kB (2 MB)
     rollupOptions: {
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
             if (id.includes('three')) {
-              return 'three-vendor'; // Separates Three.js into its own file
+              return 'three-vendor'; // Separates Three.js 3D assets
             }
-            return 'vendor'; // Separates other npm packages
+            return 'vendor'; // Separates general npm packages
           }
         },
       },
