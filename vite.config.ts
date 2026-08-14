fef1519f11ac64bd766a978fe2804c4965e05693
Priyanshu Das <Priyanshudas272006@gmail.com>
2026-08-14 15:16:23 +0530

    Fix chunk size warning limit

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tsConfigPaths from "vite-tsconfig-paths";
+import tailwindcss from "@tailwindcss/vite";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import { nitro } from "nitro/vite";
 
 export default defineConfig({
-  plugins: [react()],
-})
+  build: {
+    chunkSizeWarningLimit: 1000,
+  },
+  plugins: [
+    tanstackStart({
+      server: { 
+        entry: "server",
+        preset: "vercel",
+      },
+    }),
+    nitro(),
+    react(),
+    tsConfigPaths(),
+    tailwindcss(),
+  ],
+});
